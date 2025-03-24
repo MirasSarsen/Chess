982b9ba65ae4b1b5d6c99cffd8dc2b7acd6deb8b
Miras Sarsen <miras.frontender@gmail.com>
2025-03-24 22:39:59 +0500

    Implement modal for pawn promotion selection

--- Helper/modalCreator.js
+++ Helper/modalCreator.js
@@ -1,7 +1,18 @@
+import {
+    blackBishop,
+    blackKnight,
+    blackRook,
+    blackQueen,
+    whiteBishop,
+    whiteKnight,
+    whiteRook,
+    whiteQueen,
+} from "../Data/pieces.js";
+
 class ModalCreator {
     constructor(body) {
         if (!body) {
-            throw new Error("Пожалуйства выберите фигуру");
+            throw new Error("Пожалуйста выберите фигуру");
         }
 
         this.open = false;
@@ -13,38 +24,84 @@ class ModalCreator {
         document.body.appendChild(this.body);
         document.getElementById("root").classList.add("blur");
     }
+
+    hide() {
+        this.open = false;
+        document.body.removeChild(this.body);
+        document.getElementById("root").classList.remove("blur");
+    }
 }
 
-function pawnPromotion(color) {
+function pawnPromotion(color, callback, id) {
     const rook = document.createElement("img");
+    rook.onclick = rookCallback;
     rook.src = `../images/pieces/${color}/rook.png`;
 
     const knight = document.createElement("img");
+    knight.onclick = knightCallback;
     knight.src = `../images/pieces/${color}/knight.png`;
 
     const bishop = document.createElement("img");
+    bishop.onclick = bishopCallback;
     bishop.src = `../images/pieces/${color}/bishop.png`;
 
     const queen = document.createElement("img");
+    queen.onclick = queenCallback;
     queen.src = `../images/pieces/${color}/queen.png`;
 
     const imageContainer = document.createElement("div");
+    imageContainer.classList.add("promotion-container");
     imageContainer.appendChild(rook);
     imageContainer.appendChild(knight);
     imageContainer.appendChild(bishop);
     imageContainer.appendChild(queen);
 
     const msg = document.createElement("p");
-    msg.textContent = "Ваша пешка превращена";
+    msg.textContent = "Ваша пешка превратиться в:";
 
     const finalContainer = document.createElement("div");
     finalContainer.appendChild(msg);
     finalContainer.appendChild(imageContainer);
-
     finalContainer.classList.add("modal");
 
     const modal = new ModalCreator(finalContainer);
     modal.show();
+
+    function rookCallback() {
+        if (color == "white") {
+            callback(whiteRook, id);
+        } else {
+            callback(blackRook, id);
+        }
+        modal.hide();
+    }
+
+    function knightCallback() {
+        if (color == "white") {
+            callback(whiteKnight, id);
+        } else {
+            callback(blackKnight, id);
+        }
+        modal.hide();
+    }
+
+    function bishopCallback() {
+        if (color == "white") {
+            callback(whiteBishop, id);
+        } else {
+            callback(blackBishop, id);
+        }
+        modal.hide();
+    }
+
+    function queenCallback() {
+        if (color == "white") {
+            callback(whiteQueen, id);
+        } else {
+            callback(blackQueen, id);
+        }
+        modal.hide();
+    }
 }
 
 export default pawnPromotion;
